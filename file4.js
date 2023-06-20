7ac8de0742e02c4652355838e6de4f3f9e3c3e00
Nitinnihal <nnjha1999@gmail.com>
2023-06-21 01:39:00 +0530

    string concat and template

---
+++ file4.js
@@ -0,0 +1,29 @@
+// string concatenation.
+
+// let fName = "Nitin";
+// let mName = "Nihal";
+// let lName = "jha";
+
+// let fullName = fName + " "+ mName + " " + lName;
+// console.log(fullName);
+
+// let str1 = "20";
+// let str2 = "23";
+// let nStr = str1 + str2;
+// console.log(nStr);
+
+// nStr = +str1 + +str2;
+//  // nStr = Number(str1) + Number(str2);
+// console.log(nStr);
+
+
+
+//template string
+//  Q) my name is nitin and my age is 23.
+
+let fName = "Nitin";
+let age = 23;
+
+// let aboutMe = "my name is " + fName + " and my age is " + age + ".";
+let aboutMe = `my name is ${fName} and my age is ${age}`
+console.log(aboutMe);
