42d6f9ecc90cb360ca47b559fd3c82df8db8f024
Nitinnihal <nnjha1999@gmail.com>
2023-07-09 01:55:57 +0530

    ternary operator, AND,OR operator

---
+++ file7.js
@@ -0,0 +1,49 @@
+// ternary operator/conditional operator
+
+// let age = 25;
+// let drink;
+
+// if(age <= 15){
+//     drink = "milk";
+// }else{
+//     drink = "coffee";
+// }
+// console.log(drink);
+
+// let age = 16;
+// console.log(drink = age <= 15 ? "milk" : "coffee");
+
+
+
+// AND(&&), OR(||)  operators
+
+ let fName = "nitin";
+ let age = 23;
+
+//  if(fName[0] === "N"){
+//     console.log("your name starts with N");
+//  }
+//  if(age === 23){
+//     console.log(age);
+//  }
+
+
+
+        //  AND operator
+
+// if(fName[0]==="N" && age === 23){
+//     console.log("your name starts with N and your age is " + age);
+// }
+// else{
+//     console.log("inside else");
+// }
+
+
+    //  OR operator
+
+if(fName[0]==="N" || age === 23){
+    console.log("inside if");
+}
+else{
+    console.log("inside else");
+}
