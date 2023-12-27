3e40256af91b45513ffc1a2aa96b3b0a52c6368d
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2023-12-27 23:42:45 +0530

    example of for loop

--- file10.js
+++ file10.js
@@ -1,8 +1,21 @@
+//  while loop
 
-//  while loop                                                    
+// let i = 0;
+// while(i <= 3){
+//     console.log(i);
+//     i++;
+// }
 
-let i = 0;
-while(i <= 3){
-    console.log(i);
-    i++;
+// print sum of first n natural numbers
+let n = 0;
+let sum = 0;
+while (n <= 10) {
+  sum = sum + n;
+  n++;
 }
+console.log(sum);
+
+// using math total  = (num*(num+1))/2          *****faster than loop*******
+let num = 10;
+let total = (num * (num + 1)) / 2;
+console.log(total);
