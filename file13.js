f1021683ff73e8c35fa2ba74ac2c3cfb06cb3818
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2023-12-29 23:01:21 +0530

    DO-WHILE LOOP

---
+++ file13.js
@@ -0,0 +1,17 @@
+// DO-WHILE LOOP
+
+let i = 10;
+// while (i <= 9) {
+//   console.log(i);
+//   i++;
+// }
+// in above while loop if in the starting ,  1st condition will be false ,the loop will not get executed even a single time.
+
+do {
+  console.log(i);
+  i++;
+} while (i <= 9);
+
+console.log("the value of i is ", i);
+
+// while in this DO-WHILE loop even if the 1st condition is false still the loop will run atleast onetime.
