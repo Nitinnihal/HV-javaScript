d00d65b7789a005ccac763d415621d1a91d1abbe
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2023-12-29 23:42:12 +0530

    introduction to ARRAYS

---
+++ file14.js
@@ -0,0 +1,20 @@
+// introduction to Arrays
+
+let fruits = ["mango", "papaya", "banana", "orange"];
+let obj = {}; // object literals
+// console.log(fruits);
+// console.log(fruits[2]);
+
+// let mixed = [1, 2, 4, 3.4, "nitin", null, undefined];
+// console.log(mixed);
+
+// in javaScript arrays we can store any datatypes unlike java where we used to store only one type datatypes in an array.
+// and ARRAYS ARE MUTABLE
+
+// fruits[1] = "guava";
+// console.log(fruits);
+
+// ARRAYS are reference type variables. and all ref. types in javaScript are objects therefore ARRAYS are also object in javaScript.
+console.log(typeof obj);
+console.log(typeof fruits);
+console.log(Array.isArray(fruits)); // used to differantiate between array and other objects.
