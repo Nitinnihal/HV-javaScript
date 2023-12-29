972bebf8b8e19783e104af260911bb52916a489f
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2023-12-29 22:51:15 +0530

    Break and Continue statement

--- file12.js
+++ file12.js
@@ -2,9 +2,20 @@
 
 // BREAK KEYWORD
 
+// for (let i = 0; i <= 10; i++) {
+//   if (i == 5) {
+//     break;
+//   }
+//   console.log(i);
+// }
+// console.log("hello");
+
+// CONTINUE KEYWORD
+
 for (let i = 0; i <= 10; i++) {
   if (i == 5) {
-    break;
+    // here we are skipping 5 by usinng continue keyword coz it will not  let the lineno. 20 and afterwards get executed.instead it will again go to the for loop and hence skip the printing of 5.
+    continue;
   }
   console.log(i);
 }
