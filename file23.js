aba7161bce790004cdaf0c115b8daa8507160780
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2024-02-25 12:52:23 +0530

    for-in loop in Arrays

--- file23.js
+++ file23.js
@@ -1,13 +1,14 @@
 // ARRAY DESTRUCTURING
 
-const myArr = ["val1", "val2"];
+const myArr = ["val1", "val2", 5, "val4"];
 // const myVar1 = myArr[0];
 // const myVar2 = myArr[1];
 
 // console.log("value of myVar1 is = " + myVar1);
 // console.log("value of myVar2 is = " + myVar2);
 
-const [myVar1, myVar2] = myArr;
-
+let [myVar1, myVar2, myVar3] = myArr;
+myVar1 = 7;
 console.log("value of myVar1 is = " + myVar1);
 console.log("value of myVar2 is = " + myVar2);
+console.log("value of myVar3 is = " + myVar3);
