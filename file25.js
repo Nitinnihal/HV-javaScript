d096c03a3a26c8b1cbaea5706731bd661385ceba
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2024-02-25 14:07:00 +0530

    DOT notation vs BRACKET notation

--- file25.js
+++ file25.js
@@ -1,5 +1,15 @@
+// DOT notation vs BRACKET notation.
+
 const person = {
   name: "Nitin",
   age: 24,
-  skills: ["html5", "css3", "java", "javaScript"],
+  "person skills": ["html5", "css3", "java", "javaScript"],
 };
+// console.log(person["person skills"]);
+
+
+// let's suppose there is a variable named key and a value is stored in that key named variable.now we have to add this variable's value as a key to the object and we can give any value for this property(key) to the object. then-
+
+const key = "email";
+person[key] = "nnjha1111@gmail.com";
+console.log(person);
