90b0b363c9782575dfbb2fd9a2ec9b196b34b1ca
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2024-02-25 12:13:59 +0530

    for-in loop in Arrays

--- file22.js
+++ file22.js
@@ -4,7 +4,7 @@
 const arr = [1, 2, 3, 4, 5, 6.77, "shiv", "m"];
 const arr2 = [];
 for (let index in arr) {
-  //   console.log(index);
+  // console.log(index);
   //   console.log(arr[index].toString().toUpperCase());
   arr2.push(arr[index].toString().toUpperCase());
 }
---
+++ file23.js
@@ -0,0 +1,13 @@
+// ARRAY DESTRUCTURING
+
+const myArr = ["val1", "val2"];
+// const myVar1 = myArr[0];
+// const myVar2 = myArr[1];
+
+// console.log("value of myVar1 is = " + myVar1);
+// console.log("value of myVar2 is = " + myVar2);
+
+const [myVar1, myVar2] = myArr;
+
+console.log("value of myVar1 is = " + myVar1);
+console.log("value of myVar2 is = " + myVar2);
