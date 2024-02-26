31d62293766b8276ebbead5636b93302f71ce624
Nitin Nihal <96877954+Nitinnihal@users.noreply.github.com>
2024-02-26 14:44:59 +0530

    Basics of functions.

--- file32.js
+++ file32.js
@@ -4,10 +4,18 @@
 //   console.log("Heisenberg....!");
 // }
 
+// const sayMyName = function () {           // function expression
+//   console.log("Heisenberg....!");
+// };
+
 // sayMyName(); // calling/invoking/running a function
 
 // function twoPlusFour() {
 //   return 2 + 4;
+// }
+
+// const twoPlusFour = function (){
+//   return 2+4;
 // }
 
 // // console.log(twoPlusFour());
@@ -19,7 +27,12 @@
 // function sumTwoNumbers(a, b) {              // here a,b are termed as parameters
 //   return a + b;
 // }
-// console.log(sumTwoNumbers(2, 4));           //  here 2,4 are known as arguments
+
+// const sumTwoNumbers = function (a, b) {
+//   return a + b;
+// };
+
+// console.log(sumTwoNumbers(2, 4)); //  here 2,4 are known as arguments
 
 // write a function which takes single parameter and return true for even numbrs and false for odd numbers.
 
@@ -39,7 +52,12 @@
 // function isEven(num) {
 //   return num % 2 === 0;
 // }
-// const returnedVal = isEven(0);
+
+// const isEven = function (num) {
+//   return num % 2 === 0;
+// };
+
+// const returnedVal = isEven(4);
 // console.log(returnedVal);
 
 // write a function which takes a string as input an return it's 1st character as output.
@@ -53,13 +71,22 @@
 
 // write a function which takes an array an a target value as parameter if the target is found return a message of the index at which target is found.
 
-function search(arr, t) {
+// function search(arr, t) {
+//   for (let i = 0; i <= arr.length - 1; i++) {
+//     if (arr[i] === t) {
+//       return i;
+//     }
+//   }
+//   return "not found";
+// }
+
+const search = function (arr, t) {
   for (let i = 0; i <= arr.length - 1; i++) {
     if (arr[i] === t) {
       return i;
     }
   }
-  return "not found";
-}
+  return -1;
+};
 const Res = search([1, 2, 3, 4, 5], 8);
 console.log(Res);
